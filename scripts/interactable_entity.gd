903520c9b5832a9220c27535ca2ae2248c929e28
Zahraa Zahir <zahraazahiirr@gmail.com>
2026-03-07 14:37:06 +0300

    feat(npc): implement base interactable entity

---
+++ scripts/interactable_entity.gd
@@ -0,0 +1,51 @@
+extends CharacterBody2D
+
+@export var entity_id: String = "npc_01"
+
+@export var is_fixed: bool = false:
+	set(value):
+		is_fixed = value
+		_apply_visual_state()
+
+@onready var anim: AnimationPlayer = $AnimationPlayer
+@onready var sprite: Sprite2D = $Sprite2D
+@onready var prompt: Label = $PromptLabel
+
+var player_is_near: bool = false
+
+func _ready() -> void:
+	$InteractionZone.body_entered.connect(_on_player_entered)
+	$InteractionZone.body_exited.connect(_on_player_exited)
+	
+	_apply_visual_state()
+	prompt.visible = false
+
+func _process(_delta: float) -> void:
+	if player_is_near and not is_fixed:
+		if Input.is_action_just_pressed("interact"):
+			_simulate_fix()
+
+func _simulate_fix() -> void:
+	print("DEBUG: Manually patching ", entity_id)
+	is_fixed = true
+
+func _apply_visual_state() -> void:
+	if not is_inside_tree() or not sprite: return
+
+	if sprite.material:
+		sprite.material.set_shader_parameter("is_glitched", !is_fixed)
+
+	if is_fixed:
+		anim.play("idle_normal")
+	else:
+		anim.play("glitch_active")
+
+func _on_player_entered(body: Node2D) -> void:
+	if body.name == "Player":
+		player_is_near = true
+		if not is_fixed: prompt.visible = true
+
+func _on_player_exited(body: Node2D) -> void:
+	if body.name == "Player":
+		player_is_near = false
+		prompt.visible = false
