c12f5bdcc0c2c86c1090468601e9793dfe4895fa
Zahraa Zahir <zahraazahiirr@gmail.com>
2026-03-14 23:15:04 +0300

    docs: remove unnecessary comment

--- scripts/mouse_entity.gd
+++ scripts/mouse_entity.gd
@@ -12,9 +12,8 @@ func _perform_bloom():
 	flowers.visible = true
 	for flower in flowers.get_children():
 		flower.scale = Vector2.ZERO
-		# Create a Tween for each flower
+		
 		var tween = create_tween()
-		# TRANS_ELASTIC + EASE_OUT = The perfect 'pop'
 		tween.tween_property(flower, "scale", Vector2.ONE, 0.5)\
 			.set_trans(Tween.TRANS_ELASTIC)\
 			.set_ease(Tween.EASE_OUT)
