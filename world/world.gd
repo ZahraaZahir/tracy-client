305d6891630cbadb0f8ceed293f44a3a5af09b9e
Zahraa Zahir <zahraazahiirr@gmail.com>
2026-05-08 19:26:20 +0300

    feat(environment): add trees with collisions and add environment collision mask to bugs

--- world/world.gd
+++ world/world.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 @onready var player = $Player
+@onready var env_layer = $Environment
 const MAP_ID = "main_world"
 const BUG_SCENE = preload("res://bug/Bug.tscn")
 
@@ -31,11 +32,9 @@ func _on_world_loaded(data: Dictionary) -> void:
 		var inventory_list = data.get("inventory", [])
 		var inventory_count = inventory_list.size()
 		
-
 		for npc in get_tree().get_nodes_in_group("interactable"):
 			if npc.entity_id in fixed_ids:
 				npc.load_silent_state()
-		
 		
 		var total_npcs = data.totalEntities
 		var fixed_count = fixed_ids.size()
@@ -69,10 +68,12 @@ func _spawn_bugs(count: int) -> void:
 		var query = PhysicsPointQueryParameters2D.new()
 		query.position = target_pos
 		query.collision_mask = 1
-		
 		var intersections = space_state.intersect_point(query)
 		
-		if intersections.is_empty():
+		var map_pos = env_layer.local_to_map(env_layer.to_local(target_pos))
+		var has_env_tile = env_layer.get_cell_source_id(map_pos) != -1
+		
+		if intersections.is_empty() and not has_env_tile:
 			var bug = BUG_SCENE.instantiate()
 			
 			bug.z_index = 3           
@@ -87,7 +88,6 @@ func _spawn_bugs(count: int) -> void:
 
 	if spawned_count < count:
 		push_warning("WORLD: Limited walkable space. Only spawned %d/%d bugs." % [spawned_count, count])
-		
 
 func _on_entity_fixed(_id: String) -> void:
 	WorldService.save_state(player.global_position, MAP_ID)
